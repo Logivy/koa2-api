57d4d3d365d2fd822b113fb1ccaffe174576b30c
luojiewei <luojiewei@custouch.com>
2022-02-14 18:16:00 +0800

    update

---
+++ src/constant/err.type.js
@@ -0,0 +1,62 @@
+module.exports = {
+  userFormateError: {
+    code: '10001',
+    message: '用户名或密码为空',
+    result: ''
+  },
+  userAlreadyExisted: {
+    code: '10002',
+    message: '用户名存在',
+    result: ''
+  },
+  registerError: {
+    code: '10003',
+    message: '用户注册失败',
+    result: ''
+  },
+  userNotExisted: {
+    code: '10004',
+    message: '用户不存在',
+    result: ''
+  },
+  loginError: {
+    code: '10005',
+    message: '用户登录失败',
+    result: ''
+  },
+  invalidPassword: {
+    code: '10006',
+    message: '密码不匹配',
+    result: ''
+  },
+  tokenExpiredError: {
+    code: '10101',
+    message: 'token已过期',
+    result: ''
+  },
+  invalidToken: {
+    code: '10102',
+    message: '无效token',
+    result: ''
+  },
+  modifyPasswordError: {
+    code: '10103',
+    message: '修改密码失败',
+    result: ''
+  },
+  hasNotAdminPermissionError: {
+    code: '10104',
+    message: '没有管理员权限',
+    result: ''
+  },
+  unSupportFileTypeError: {
+    code: '10201',
+    message: '商品图片上传失败',
+    result: ''
+  },
+  fileUploadError: {
+    code: '10202',
+    message: '不支持的文件格式',
+    result: ''
+  }
+}
